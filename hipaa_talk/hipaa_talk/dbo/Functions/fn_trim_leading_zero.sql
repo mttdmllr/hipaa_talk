96bec49abbf0c162210410f7e7367d8448e74c53
mttd <mttdmllr@gmail.com>
2017-02-28 08:47:14 -0800

    local reset again

--- hipaa_talk/hipaa_talk/dbo/Functions/fn_trim_leading_zero.sql
+++
@@ -1,6 +0,0 @@
-﻿
-create FUNCTION [dbo].[fn_trim_leading_zero] (@varIn money)
-RETURNS varchar(max) AS
-BEGIN
-	return dbo.fn_LTrimPat(replace(@varIn, '-0.', '-.'), '0')
-END
--- hipaa_talk/hipaa_talk/dbo/Views/vw_x12_transaction_schema.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE VIEW dbo.vw_x12_transaction_schema
-AS
-SELECT     x12_transaction_schema_id, x12_transaction_schema_ud, x12_transaction_schema_nm, schema_namespace, standards_version, 
-                      transaction_id_code, functional_group_id_code, functional_version_id_code, table_nm, active, modified_date, created_date
-FROM         dbo.x12_transaction_schema
-
